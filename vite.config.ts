d8bf25f2d1a5ba64d8804507803a3f96459db27b
Himanshu Sharma <himanshamra@gmail.com>
2025-10-14 18:10:44 -0400

    Fixed some deploy issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/", 
+  base: "F1-Race-Pace", 
   test: {
     environment: "jsdom",
     setupFiles: "./vitest.setup.ts"
